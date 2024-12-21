59a6c135ba92c559ce1d3089d21a7eadd63a77db
Aloïs Van de Voorde <vandevoorde.alois@gmail.com>
2024-12-21 12:16:40 +0100

    day 14 solved

---
+++ Day14-RestroomRedoubt/AOC14-2.py
@@ -0,0 +1,198 @@
+
+'''
+--- Day 14: Restroom Redoubt ---
+One of The Historians needs to use the bathroom; fortunately, you know there's a bathroom near an unvisited location on their list, and so you're all quickly teleported directly to the lobby of Easter Bunny Headquarters.
+
+Unfortunately, EBHQ seems to have "improved" bathroom security again after your last visit. The area outside the bathroom is swarming with robots!
+
+To get The Historian safely to the bathroom, you'll need a way to predict where the robots will be in the future. Fortunately, they all seem to be moving on the tile floor in predictable straight lines.
+
+You make a list (your puzzle input) of all of the robots' current positions (p) and velocities (v), one robot per line. For example:
+
+p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3
+Each robot's position is given as p=x,y where x represents the number of tiles the robot is from the left wall and y represents the number of tiles from the top wall (when viewed from above). So, a position of p=0,0 means the robot is all the way in the top-left corner.
+
+Each robot's velocity is given as v=x,y where x and y are given in tiles per second. Positive x means the robot is moving to the right, and positive y means the robot is moving down. So, a velocity of v=1,-2 means that each second, the robot moves 1 tile to the right and 2 tiles up.
+
+The robots outside the actual bathroom are in a space which is 101 tiles wide and 103 tiles tall (when viewed from above). However, in this example, the robots are in a space which is only 11 tiles wide and 7 tiles tall.
+
+The robots are good at navigating over/under each other (due to a combination of springs, extendable legs, and quadcopters), so they can share the same tile and don't interact with each other. Visually, the number of robots on each tile in this example looks like this:
+
+1.12.......
+...........
+...........
+......11.11
+1.1........
+.........1.
+.......1...
+These robots have a unique feature for maximum bathroom security: they can teleport. When a robot would run into an edge of the space they're in, they instead teleport to the other side, effectively wrapping around the edges. Here is what robot p=2,4 v=2,-3 does for the first few seconds:
+
+Initial state:
+...........
+...........
+...........
+...........
+..1........
+...........
+...........
+
+After 1 second:
+...........
+....1......
+...........
+...........
+...........
+...........
+...........
+
+After 2 seconds:
+...........
+...........
+...........
+...........
+...........
+......1....
+...........
+
+After 3 seconds:
+...........
+...........
+........1..
+...........
+...........
+...........
+...........
+
+After 4 seconds:
+...........
+...........
+...........
+...........
+...........
+...........
+..........1
+
+After 5 seconds:
+...........
+...........
+...........
+.1.........
+...........
+...........
+...........
+The Historian can't wait much longer, so you don't have to simulate the robots for very long. Where will the robots be after 100 seconds?
+
+In the above example, the number of robots on each tile after 100 seconds has elapsed looks like this:
+
+......2..1.
+...........
+1..........
+.11........
+.....1.....
+...12......
+.1....1....
+To determine the safest area, count the number of robots in each quadrant after 100 seconds. Robots that are exactly in the middle (horizontally or vertically) don't count as being in any quadrant, so the only relevant robots are:
+
+..... 2..1.
+..... .....
+1.... .....
+           
+..... .....
+...12 .....
+.1... 1....
+In this example, the quadrants contain 1, 3, 4, and 1 robot. Multiplying these together gives a total safety factor of 12.
+
+Predict the motion of the robots in your list within a space which is 101 tiles wide and 103 tiles tall. What will the safety factor be after exactly 100 seconds have elapsed?
+211692000
+'''
+import re
+amount_of_rows = 103
+amount_of_columns = 101
+
+def read_input(file):
+    robots = []
+    with open(file) as f:
+        for line in f:
+            numbers = re.findall(r'-?\d+', line)
+            robots.append({'x': int(numbers[0]), 'y': int(numbers[1]), 'vx': int(numbers[2]), 'vy': int(numbers[3]), 'endx': int(numbers[0]), 'endy': int(numbers[1])})
+    return robots
+
+
+def fast_move(robots, amount):
+    for robot in robots:
+        robot['endx'] = (robot['x'] + robot['vx'] * amount) % 101
+        robot['endy'] = (robot['y'] + robot['vy'] * amount) % 103
+    
+def count_robots(robots):
+    q1 = q2 = q3 = q4 = 0
+    for robot in robots:
+        if robot['endx'] < 50:
+            if robot['endy'] < 51:
+                q1 += 1
+            elif robot['endy'] > 51:
+                q4 += 1
+        elif robot['endx'] > 50:
+            if robot['endy'] < 51:
+                q2 += 1
+            elif robot['endy'] > 51:
+                q3 += 1
+    return q1 * q2 * q3 * q4
+
+
+def count_robots_in_a_row(robots):
+    max_count = 0
+    for i in range(amount_of_rows):
+        robots_in_row = []
+        for robot in robots:
+            if robot['endy'] == i:
+                robots_in_row.append(robot)
+        robots_in_row.sort(key=lambda x: x['endx'])
+        max_row_count = 0
+        continuous_count = 1
+        start_pos = 0
+        for robot in robots_in_row:
+            if robot['endx'] == start_pos + 1:
+                continuous_count += 1
+            else:
+                if continuous_count > max_row_count:
+                    max_row_count = continuous_count
+                continuous_count = 1
+            start_pos = robot['endx']                
+        if continuous_count > max_count:
+            max_count = continuous_count
+    return max_count
+
+def print_map(robots):
+    for i in range(amount_of_rows):
+        line = ''
+        for j in range(amount_of_columns):
+            found = False
+            amount = 0
+            for robot in robots:
+                if robot['endx'] == j and robot['endy'] == i:
+                    found = True
+                    amount += 1
+            line += str(amount) if found else '.'
+        print(line)
+
+def main():
+    robots = read_input('input.txt')
+    for i in range(10000):
+        fast_move(robots,i)
+        if count_robots_in_a_row(robots) > 20:
+            print(f"Seconds: {i}")
+            print_map(robots)
+            return
+
+main()
